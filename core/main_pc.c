38358f6ca03436076a557083ed9ab76c10423da3
Paweł Bolek <pawellekb@gmail.com>
2026-02-12 23:27:44 +0100

    Allow for custom config to be passed

--- core/main_pc.c
+++ core/main_pc.c
@@ -7,6 +7,27 @@
 
 TrafficSystem sys;
 
+void handle_config() {
+    PayloadConfig payload;
+    size_t read_count = fread(&payload, sizeof(PayloadConfig), 1, stdin);
+    
+    if (read_count != 1) {
+        fprintf(stderr, "[C-ERR] Failed to read Config payload\n");
+        return;
+    }
+
+    TimingConfig config = {
+        .green_st = payload.green_st,
+        .green_lt = payload.green_lt,
+        .yellow = payload.yellow,
+        .all_red = payload.all_red
+    };
+    
+    traffic_init(&sys, config);
+    fprintf(stderr, "[C-OK] Config loaded: ST=%d, LT=%d, Y=%d, AR=%d\n",
+            config.green_st, config.green_lt, config.yellow, config.all_red);
+}
+
 void handle_add_vehicle() {
     PayloadAddVehicle payload;
     size_t read_count = fread(&payload, sizeof(PayloadAddVehicle), 1, stdin);
@@ -20,14 +41,27 @@ void handle_add_vehicle() {
 }
 
 void handle_step() {
-    // TODO: traffic_fsm_step(); and rest of response 
-    sys.current_step++; 
+    char discharged_ids[ROAD_COUNT * LANES_PER_ROAD][VEHICLE_ID_LEN];
+    memset(discharged_ids, 0, sizeof(discharged_ids));
+
+    int count = traffic_fsm_step(&sys, discharged_ids);
 
     ResponseStep resp;
     resp.current_step = sys.current_step;
-    resp.vehicles_left_count = 0;
+    resp.current_state = (uint8_t)sys.current_state;
+    resp.light_ns_st = (uint8_t)sys.lights[NORTH][LANE_STRAIGHT_RIGHT];
+    resp.light_ns_lt = (uint8_t)sys.lights[NORTH][LANE_LEFT];
+    resp.light_ew_st = (uint8_t)sys.lights[EAST][LANE_STRAIGHT_RIGHT];
+    resp.light_ew_lt = (uint8_t)sys.lights[EAST][LANE_LEFT];
+    resp.vehicles_out = (uint16_t)count;
 
     fwrite(&resp, sizeof(ResponseStep), 1, stdout);
+
+    if (count > 0) {
+        fwrite(discharged_ids, VEHICLE_ID_LEN, count, stdout);
+    }
+    
+    fflush(stdout);
 }
 
 int main() {
@@ -35,11 +69,16 @@ int main() {
     setvbuf(stdout, NULL, _IONBF, 0);
     setvbuf(stdin, NULL, _IONBF, 0);
 
-    traffic_init(&sys);
-    
+    TimingConfig default_config = DEFAULT_TIMING;
+    traffic_init(&sys, default_config);
+
     CmdHeader header;
     while (fread(&header, sizeof(CmdHeader), 1, stdin) == 1) {
         switch (header.cmd_type) {
+            case CMD_CONFIG:
+                handle_config();
+                break;
+                
             case CMD_ADD_VEHICLE:
                 handle_add_vehicle();
                 break;
--- core/protocol.h
+++ core/protocol.h
@@ -5,10 +5,18 @@
 #include "traffic_queue.h"
 
 typedef enum {
+    CMD_CONFIG = 0,
     CMD_ADD_VEHICLE = 1,
     CMD_STEP = 2,
     CMD_STOP = 99
 } CommandType;
+
+typedef struct __attribute__((packed)) {
+    uint32_t green_st;
+    uint32_t green_lt;
+    uint32_t yellow;
+    uint32_t all_red;
+} PayloadConfig;
 
 typedef struct __attribute__((packed)) {
     uint8_t cmd_type;
@@ -23,7 +31,14 @@ typedef struct __attribute__((packed)) {
 
 typedef struct __attribute__((packed)) {
     uint32_t current_step;
-    uint16_t vehicles_left_count;
+    uint8_t current_state;
+    
+    uint8_t light_ns_st;
+    uint8_t light_ns_lt;
+    uint8_t light_ew_st;
+    uint8_t light_ew_lt;
+    
+    uint16_t vehicles_out;      
 } ResponseStep;
 
 #endif
